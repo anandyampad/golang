5b512a940f07faeb6ac42f45d2cb4e960d15f5d8
anandk4 <anandk4@cisco.com>
2022-12-12 12:15:30 +0530

    Commit 2

--- program.go
+++ program.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	print("Hello World!\n")
+
+	var x, y int = 10, 20
+	sum := x + y
+
+	fmt.Printf("sum of %d and %d is %d\n", x, y, sum)
+
+	const (
+		red = iota + 4
+		blue
+		green
+	)
+	fmt.Printf("red = %d blue = %d green = %d\n", red, blue, green)
+
 }
