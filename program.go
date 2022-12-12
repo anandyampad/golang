110b55ea13dc740c7be9624fa295a09080d6a927
anandk4 <anandk4@cisco.com>
2022-12-12 12:31:38 +0530

    loop examples

--- program.go
+++ program.go
@@ -3,18 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	print("Hello World!\n")
+	for i := 0; i < 5; i++ {
+		fmt.Println("Number is ", i)
+	}
 
-	var x, y int = 10, 20
-	sum := x + y
+	for sum := 1; sum < 100; {
+		fmt.Printf("%d is less than 100\n", sum)
+		sum += sum
+	}
 
-	fmt.Printf("sum of %d and %d is %d\n", x, y, sum)
+	sum := 1
+	for {
+		fmt.Printf("%d is less than 100\n", sum)
+		sum += sum
+		if sum > 100 {
+			break
+		}
+	}
 
-	const (
-		red = iota + 4
-		blue
-		green
-	)
-	fmt.Printf("red = %d blue = %d green = %d\n", red, blue, green)
-
+LOOP:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			fmt.Printf("i=%d j=%d\n", i, j)
+			if i == j {
+				fmt.Println("==================")
+				continue LOOP
+			}
+		}
+	}
 }
